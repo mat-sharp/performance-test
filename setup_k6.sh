16f92b06582c6fd9485769b8641972d309561357
Mat Sharp <mat.sharp@icloud.com>
2020-05-07 12:50:32 +0100

    Update setup_k6.sh

--- setup_k6.sh
+++ setup_k6.sh
@@ -4,10 +4,10 @@ set -ex
 install_k6() {
     
 
-    sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
+    apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv-keys 379CE192D401AB61
     echo "deb https://dl.bintray.com/loadimpact/deb stable main" | sudo tee -a /etc/apt/sources.list
-    sudo apt-get update
-    sudo apt-get install k6
+    apt-get update
+    apt-get install k6
 }
 
 main() {
