b56d1f4b826f6f798cd7d6d2e8e0f5941618ee26
Mat Sharp <mat.sharp@icloud.com>
2020-05-09 13:58:10 +0100

    ch ch ch changes

--- performance/test.js
+++ performance/test.js
@@ -8,7 +8,7 @@ export let options = {
     duration: "1m",
     vus: 50,
     thresholds: {
-        simple_request_duration: ["p(95)<120"]
+        simple_request_duration: ["p(95)<100"]
     }
 };
 
